276c7488ebd05b486c98ac0de67c4321870c6759
QLeelulu <qleelulu@gmail.com>
2012-12-18 21:10:27 +0800

    c

--- golink/config.go
+++ golink/config.go
@@ -32,7 +32,7 @@ var (
 )
 
 var Config *goku.ServerConfig = &goku.ServerConfig{
-    Addr:           ":80",
+    Addr:           ":8080",
     ReadTimeout:    10 * time.Second,
     WriteTimeout:   10 * time.Second,
     MaxHeaderBytes: 1 << 20,
