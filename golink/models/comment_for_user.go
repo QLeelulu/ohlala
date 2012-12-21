61c810e12820335d326df8c86c32492e99a6b41e
QLeelulu <qleelulu@gmail.com>
2012-12-21 23:09:11 +0800

    评论推送与提醒

--- golink/models/comment_for_user.go
+++ golink/models/comment_for_user.go
@@ -1,10 +1,11 @@
 package models
 
 import (
-// // "bytes"
-// "errors"
-// "fmt"
-// "github.com/QLeelulu/goku"
+    // // "bytes"
+    // "errors"
+    // "fmt"
+    "github.com/QLeelulu/goku"
+
 // "github.com/QLeelulu/goku/form"
 // "github.com/QLeelulu/ohlala/golink"
 // "github.com/QLeelulu/ohlala/golink/utils"
@@ -12,3 +13,24 @@ import (
 // // "strings"
 // "time"
 )
+
+var table_CommentForUser string = "comment_for_user"
+
+func CommentForUser_Add(userId int64, comment Comment) error {
+    var db *goku.MysqlDB = GetDB()
+    defer db.Close()
+
+    m := map[string]interface{}{}
+
+    m["user_id"] = userId
+    m["comment_id"] = comment.Id
+    m["link_id"] = comment.LinkId
+    m["pcomment_id"] = comment.ParentId
+    m["create_time"] = comment.CreateTime
+
+    _, err := db.Insert(table_CommentForUser, m)
+    if err != nil {
+        goku.Logger().Errorln(err.Error())
+    }
+    return err
+}
