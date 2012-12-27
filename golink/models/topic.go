c108d14b98d1cd4e79f197a52e0553929c6634cd
QLeelulu <qleelulu@gmail.com>
2012-12-28 00:49:21 +0800

    topic default icon

--- golink/models/topic.go
+++ golink/models/topic.go
@@ -25,7 +25,8 @@ type Topic struct {
 func (t Topic) PicPath(size int) string {
     if t.Pic == "" {
         // return "/assets/img/avatar/topic/topic_default.png"
-        return "http://milnk.topics.qiniudn.com/topic_default.png"
+        // return "http://milnk.topics.qiniudn.com/topic_default.png"
+        return fmt.Sprintf("http://milnk.topics.qiniudn.com/topic_default.jpg!%d", size)
     }
     // return "/assets/img/avatar/topic/" + t.Pic
     if size > 0 {
