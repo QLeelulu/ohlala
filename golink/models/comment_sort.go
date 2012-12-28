c9d86c0c277c3491cd5e85287f7352b5fcfccc90
QLeelulu <qleelulu@gmail.com>
2012-12-28 23:53:51 +0800

    Markdown

--- golink/models/comment_sort.go
+++ golink/models/comment_sort.go
@@ -46,6 +46,7 @@ func init() {
     extensions |= blackfriday.EXTENSION_AUTOLINK
     extensions |= blackfriday.EXTENSION_STRIKETHROUGH
     extensions |= blackfriday.EXTENSION_SPACE_HEADERS
+    extensions |= blackfriday.EXTENSION_HARD_LINE_BREAK
 
     // var output []byte
     // output = blackfriday.Markdown(input, renderer, extensions)
