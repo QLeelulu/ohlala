7100b5adcb8ba7c5148324948f11ec46a5e890fe
QLeelulu <qleelulu@gmail.com>
2013-01-07 00:37:46 +0800

    顶部导航栏

--- golink/middlewares/util-middleware.go
+++ golink/middlewares/util-middleware.go
@@ -15,7 +15,8 @@ func (tmd *UtilMiddleware) OnBeginRequest(ctx *goku.HttpContext) (goku.ActionRes
 }
 
 func (tmd *UtilMiddleware) OnBeginMvcHandle(ctx *goku.HttpContext) (goku.ActionResulter, error) {
-    getUser(ctx)
+    user := getUser(ctx)
+    getTopNavTopics(ctx, user)
     return nil, nil
 }
 func (tmd *UtilMiddleware) OnEndMvcHandle(ctx *goku.HttpContext) (goku.ActionResulter, error) {
@@ -26,7 +27,7 @@ func (tmd *UtilMiddleware) OnEndRequest(ctx *goku.HttpContext) (goku.ActionResul
     return nil, nil
 }
 
-func getUser(ctx *goku.HttpContext) {
+func getUser(ctx *goku.HttpContext) *models.User {
     c, err := ctx.Request.Cookie("_glut")
     if err == nil {
         user, _ := models.User_GetByTicket(c.Value)
@@ -34,6 +35,38 @@ func getUser(ctx *goku.HttpContext) {
             ctx.Data["user"] = user
             // 暂时先设置到ViewData里面吧，应该需要一个更好的办法？
             ctx.ViewData["user"] = user
+            return user
         }
     }
+    return nil
+}
+
+// 顶部导航栏的话题列表。
+// 如果用户已经登陆，则获取用户关注的话题，
+// 如果未登陆则获取全站的最流行话题列表.
+func getTopNavTopics(ctx *goku.HttpContext, user *models.User) {
+    var topics []models.Topic
+    if user == nil {
+        topics, _ = models.Topic_GetTops(1, 20)
+    } else {
+        tuser, _ := models.User_GetFollowTopics(user.Id, 1, 20, "link_count desc")
+        if len(tuser) < 20 {
+            // 不够20条，则合并
+            tall, _ := models.Topic_GetTops(1, 20)
+            topics = make([]models.Topic, 0, len(tall))
+            tmp := map[string]bool{}
+            for _, v := range tuser {
+                tmp[v.Name] = true
+            }
+            topics = append(topics, tuser...)
+            for _, v := range tall {
+                if _, ok := tmp[v.Name]; !ok {
+                    topics = append(topics, v)
+                }
+            }
+        } else {
+            topics = tuser
+        }
+    }
+    ctx.ViewData["TopNavTopics"] = topics
 }
