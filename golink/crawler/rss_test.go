037059cbc32da7e00b159494b5db58325ded0fab
jjyy <jjyy@163.com>
2014-03-02 19:17:00 +0800

    Rss爬取: 使用第三方Rss包

--- golink/crawler/rss_test.go
+++ golink/crawler/rss_test.go
@@ -9,7 +9,7 @@ import (
 func TestRssCrawler(t *testing.T) {
     rssc := RssCrawler{}
     rssc.Name = "网易头条"
-    rssc.Url = "http://www.ruanyifeng.com/blog/atom.xml"
+    rssc.Url = "http://news.163.com/special/00011K6L/rss_newstop.xml"
     rssc.UserIds = []int64{10000, 10001, 10002, 10003}
 
     err := rssc.Run()
